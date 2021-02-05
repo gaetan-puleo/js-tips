c3fdfaa7644542fc3b05b57389e3a7cd8107118d
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-02-05 23:19:08 +0100

    feat: add "remove-duplicates"

---
+++ tips/remove-duplicates.js
@@ -0,0 +1,13 @@
+// This array has a duplicated value
+const arrayWithDuplicateValue = [1,2,3,4,4,5];
+
+// To remove the duplicate value you can use a Set
+const set = new Set(arrayWithDuplicateValue)
+
+// Now the constant set contains `Set(5){ 1, 2, 3, 4, 5 }`
+// Only one `4` remains in the Set
+// Now we need to convert the Set back to an array
+const arrayResult = Array.from(set);
+
+// And the oneliner version
+const arrayResultOneLiner = [... new Set([1,2,3,4,4,5])];
