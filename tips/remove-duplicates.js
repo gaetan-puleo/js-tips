f0113051c6e24a449f882baad13d6d5931b6fd53
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-02-07 21:11:26 +0100

    chore: fix a typo

--- tips/remove-duplicates.js
+++ tips/remove-duplicates.js
@@ -1,4 +1,4 @@
-// This array has a duplicated value
+// This array has a duplicate value
 const arrayWithDuplicateValue = [1,2,3,4,4,5];
 
 // To remove the duplicate value you can use a Set
@@ -8,6 +8,7 @@ const set = new Set(arrayWithDuplicateValue)
 // Only one `4` remains in the Set
 // Now we need to convert the Set back to an array
 const arrayResult = Array.from(set);
+console.log(arrayResult);
 
 // And the oneliner version
 const arrayResultOneLiner = [... new Set([1,2,3,4,4,5])];
